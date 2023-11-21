b20a28a8af89975c6c18ad36a66a3bc2655eaa1f
Estebaanfig02 <estebanfig29@gmail.com>
2023-11-20 21:39:14 -0300

    de arreglo el main en tablet y el header

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 
 app.listen(3000,()=> {console.log("Server abierto")})
@@ -8,3 +12,12 @@ app.use(express.static("public"))
 app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"/views/home.html"))})
 app.get("/registro",(req,res)=>{res.sendFile(path.join(__dirname,"/views/register.html"))})
 app.get("/inicio-sesion",(req,res)=>{res.sendFile(path.join(__dirname,"/views/login.html"))})
+
+app.post("/register", (req,res) => {
+    console.log(req.body)
+    res.redirect("/inicio-sesion")
+});
+app.post("/login", (req,res) => {
+    console.log(req.body)
+    res.redirect("/")
+});
